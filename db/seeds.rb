cb8aefe237d350b8d724c292ebf177a77e13abed
MrBabon <christophedanna06@gmail.com>
2023-06-15 17:40:22 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+List.create(name: "Drama")
+List.create(name: "All time favourites")
+List.create(name: "Girl Power")
